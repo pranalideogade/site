5fee6d098daaf945e58e7126c59f85492f9d8de0
pranali_deogade <pranali_deogade@persistent.co.in>
2017-12-14 18:06:11 +0530

    firstcommit

---
+++ app/src/main/java/com/example/pranali_deogade/site/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.pranali_deogade.site;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://mnkydluffyy58.wordpress.com");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+
+}
